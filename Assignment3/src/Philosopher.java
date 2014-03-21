ed478e22505936f31a7d771997d7b9e4cdba948a
set000 <setrag.k@gmail.com>
2014-03-21 03:42:05 -0400

    fixed philosopher

--- Assignment3/src/Philosopher.java
+++ Assignment3/src/Philosopher.java
@@ -25,9 +25,11 @@ public class Philosopher extends BaseThread
 	{
 		try
 		{
-			// ...
+			System.out.println("Philosopher " + this.getTID() + " has started eating.");
+			yield();
 			sleep((long)(Math.random() * TIME_TO_WASTE));
-			// ...
+			yield();
+			System.out.println("Philosopher " + this.getTID() + " has finished eating.");
 		}
 		catch(InterruptedException e)
 		{
@@ -47,7 +49,20 @@ public class Philosopher extends BaseThread
 	 */
 	public void think()
 	{
-		// ...
+		try
+		{
+			System.out.println("Philosopher " + this.getTID() + " has started thinking.");
+			yield();
+			sleep((long)(Math.random() * TIME_TO_WASTE));
+			yield();
+			System.out.println("Philosopher " + this.getTID() + " has finished thinking.");
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println("Philosopher.think():");
+			DiningPhilosophers.reportException(e);
+			System.exit(1);
+		}
 	}
 
 	/**
@@ -60,11 +75,11 @@ public class Philosopher extends BaseThread
 	 */
 	public void talk()
 	{
-		// ...
-
-		saySomething();
-
-		// ...
+			System.out.println("Philosopher " + this.getTID() + " has started talking.");
+			yield();
+			saySomething();
+			yield();
+			System.out.println("Philosopher " + this.getTID() + " has finished talking.");
 	}
 
 	/**
